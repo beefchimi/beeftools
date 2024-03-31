785289c3112bebf9ef4075a97a6110f1e5627818
beefchimi <curtdulmage@gmail.com>
2024-03-31 14:54:01 -0400

    :tada: [Repo] Copy over most of the prototype code

--- src/tests/beeftools.test.ts
+++
@@ -1,5 +0,0 @@
-import {describe, it} from 'vitest';
-
-describe('beeftools', () => {
-  it.todo('write tests...');
-});
---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export type Primitive =
+  | string
+  | number
+  | bigint
+  | boolean
+  | symbol
+  | undefined
+  | null;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Builtin = Primitive | Function | Date | Error | RegExp;
+
+export type AnyObj = Record<string, unknown>;
+export type AnyFn = (...args: any[]) => any;
+export type Fn = () => void;
+
+export type GlobalEventCallback = (event: any) => void;
+export type GlobalEventTarget =
+  | Document
+  | Window
+  | HTMLElement
+  | null
+  | undefined;
+
+export type UtcMilliseconds = ReturnType<typeof Date.now>;
+
+export type IntervalId = number | ReturnType<typeof setInterval>;
+export type TimeoutId = number | ReturnType<typeof setTimeout>;
