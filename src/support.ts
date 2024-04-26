d37c5fe2c5da5f871304dccd06bef9212af5d013
beefchimi <curtdulmage@gmail.com>
2024-04-26 16:10:00 -0400

    :sparkles: [Support] Add a new supportUUID() utility

--- src/support.ts
+++ src/support.ts
@@ -9,17 +9,17 @@ export function supportDom() {
   );
 }
 
-export function supportNavigator() {
-  return (
-    typeof navigator !== 'undefined' && typeof navigator.userAgent === 'string'
-  );
-}
-
 export function supportMatchMedia() {
   return (
     supportDom() &&
     'matchMedia' in window &&
     typeof window.matchMedia === 'function'
+  );
+}
+
+export function supportNavigator() {
+  return (
+    typeof navigator !== 'undefined' && typeof navigator.userAgent === 'string'
   );
 }
 
@@ -31,5 +31,16 @@ export function supportSafari() {
   return (
     supportNavigator() &&
     /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+  );
+}
+
+export function supportUUID() {
+  // It is critical that the first `window` condition be isolated,
+  // otherwise the server will throw a `ReferenceError`.
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+    typeof window !== 'undefined' &&
+    typeof window.crypto?.randomUUID !== 'undefined'
   );
 }
