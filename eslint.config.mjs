f21282717c5b171c2f721eeae03713ce84e2af64
beefchimi <curtdulmage@gmail.com>
2024-05-24 15:18:49 -0400

    :alembic: [Eslint] Attempt to configure flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,27 @@
-{
-  "env": {
-    "browser": true,
-    "es2022": true,
+import globals from 'globals';
+import configLove from 'eslint-config-love';
+// Includes both `config` and `plugin`.
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ...configLove,
+    languageOptions: {
+      ...configLove.languageOptions,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    files: ['*.ts', '*.mjs'],
   },
-  "extends": ["love", "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+  pluginPrettier,
+  {
+    rules: {
+      'no-console': 'warn',
+      // '@typescript-eslint/explicit-function-return-type': 'off',
+      // '@typescript-eslint/strict-boolean-expressions': 'off',
+    },
   },
-  "rules": {
-    "no-console": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-  },
-}
+];
